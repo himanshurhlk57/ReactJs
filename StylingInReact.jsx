7abafa053caf00fbc0c34878c85b53d30dc29eac
Himanshu Kumar <60094122+himanshurhlk57@users.noreply.github.com>
2022-03-20 12:14:49 +0530

    Update StylingInReact.jsx

--- StylingInReact.jsx
+++ StylingInReact.jsx
@@ -1,1 +1,33 @@
 
+import React, { useState } from "react";
+
+function ChangebackgroundColor() {
+  
+   const name = "Himanshu Kumar";
+  
+   const stylingProperty = {
+    color: "blue",
+    backgroundColor: "yellow",
+    textShadow: "0px 2px 4px #ffe9c5",
+    fontFamily: '"Josefin Sans", sanse-serif',
+  }; 
+
+  
+  let color = "white";
+  const [currentColor, setColor] = useState(color);
+  const changeBackgroundColor = () => {
+    color = "#8e44ad";
+    setColor(color);
+  };
+  return (
+    <>
+     <h1 style={{ color: "red", backgroundColor: "black" }}> My name is {name} </h1>
+      <h1 style={stylingProperty}>My name is {name}</h1>
+      <div style={{ backgroundColor: currentColor }}>
+        <button onClick={changeBackgroundColor}>Change bg color</button>
+      </div>
+    </>
+  );
+}
+
+export default DigitalClock;
