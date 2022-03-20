9ad3de53f3704227acbb0136fe915787b7e2a7fb
Himanshu Kumar <60094122+himanshurhlk57@users.noreply.github.com>
2022-03-20 16:43:44 +0530

    Update contextApi.jsx

--- contextApi.jsx
+++ contextApi.jsx
@@ -1,1 +1,58 @@
 
+context
+provider
+consumer
+
+Lets pass data directly from componentA to componentC the data i want to pass is lastname
+
+import { createContext } from "react";
+import ComB from "./ComB";
+
+const LastName = createContext();
+
+// LastName context have two property one
+// is provider and second is consumer
+
+const ComA = () => {
+  return (
+    <LastName.Provider value={"Kumar"}>
+      <ComB />;
+    </LastName.Provider>
+  );
+};
+
+export default ComA;
+export { LastName };
+
+
+import ComC from "./ComC";
+
+const ComB = () => {
+  return (
+    <div>
+      <ComC />
+    </div>
+  );
+};
+
+export default ComB;
+
+
+
+import { LastName } from "./ComA";
+
+const ComC = () => {
+  return (
+    <div>
+      <LastName.Consumer>
+        {(lastName) => {
+          return <h1>This is himanshu {lastName}</h1>;
+        }}
+      </LastName.Consumer>
+    </div>
+  );
+};
+
+export default ComC;
+
+
