9ada17cd7f77a8c4064a4ae7f6a22da131c8bd47
Himanshu Kumar <60094122+himanshurhlk57@users.noreply.github.com>
2022-03-20 23:25:22 +0530

    Update PassingProps.jsx

--- PassingProps.jsx
+++ PassingProps.jsx
@@ -46,3 +46,32 @@ const ChildComponentChild = ({blogs, title}) => {
     </div>
   );
 };
+
+
+
+
+import { useState } from 'react';
+
+function MyButton({ count, onClick }) {
+  return (
+    <button onClick={onClick}>
+      Clicked {count} times
+    </button>
+  );
+}
+
+export default function MyApp() {
+  const [count, setCount] = useState(0);
+
+  function handleClick() {
+    setCount(count + 1);
+  }
+
+  return (
+    <div>
+      <h1>Counters that update together</h1>
+      <MyButton count={count} onClick={handleClick} />
+      <MyButton count={count} onClick={handleClick} />
+    </div>
+  );
+}
