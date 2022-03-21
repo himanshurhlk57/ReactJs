857c63449187303e0af7613d4d1f2f5f1fc65b1d
Himanshu Kumar <60094122+himanshurhlk57@users.noreply.github.com>
2022-03-21 09:52:13 +0530

    Update CondtionalRendering.jsx

--- CondtionalRendering.jsx
+++ CondtionalRendering.jsx
@@ -13,6 +13,23 @@ export default Practise;
 
 
 
+
+function Greeting(props) {
+  const isLoggedIn = props.isLoggedIn;
+  if (isLoggedIn) {
+    return <UserGreeting />;
+  }
+  return <GuestGreeting />;
+}
+
+ReactDOM.render(
+  // Try changing to isLoggedIn={true}:
+  <Greeting isLoggedIn={false} />,
+  document.getElementById('root')
+);
+
+
+
 let content;
 if (isLoggedIn) {
   content = <AdminPanel />;
